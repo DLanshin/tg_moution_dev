a1880b7290eb7b976b827498693571e45571fbe3
DaniilLanshinSPB <d.lanshin@finlead.team>
2022-07-25 14:54:26 +0300

    добавил мелкие правки

--- markup/components/fx-ctrl-panel/fx-ctrl-panel.js
+++ markup/components/fx-ctrl-panel/fx-ctrl-panel.js
@@ -1,12 +1,12 @@
+let controlPanel = document.getElementById("fix-control");
 let toTopBtn = document.getElementById("to-top");
-
 // When the user scrolls down 20px from the top of the document, show the button
 
 export function goToTopInit() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        toTopBtn.style.display = "flex";
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        controlPanel.style.display = "flex";
     } else {
-        toTopBtn.style.display = "none";
+        controlPanel.style.display = "none";
     }
 }
 
